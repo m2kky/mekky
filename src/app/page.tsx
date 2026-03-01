2bdeaccbd7965cf5e71e3f1f3f24ec83fc7654fc
m2kky <Muhammedmekky4@gmail.com>
2026-03-01 10:33:05 +0200

    das

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,6 @@ import MotivationSection from '@/components/MotivationSection';
 import FooterSection from '@/components/FooterSection';
 import StickyCTA from '@/components/StickyCTA';
 import ImpactTransitionSection from '@/components/ui/ImpactTransitionSection';
-import IntegrationSection from '@/components/sections/IntegrationSection';
-import EcosystemSection from '@/components/sections/EcosystemSection';
 import { createClient } from '@/utils/supabase/server';
 
 export default async function Home() {
@@ -56,9 +54,6 @@ export default async function Home() {
             "ARE YOU READY TO TAKE TRUE CONTROL?"
           ]}
         />
-
-        <IntegrationSection />
-        <EcosystemSection />
 
         <TestimonialsSection />
         <FooterSection />
